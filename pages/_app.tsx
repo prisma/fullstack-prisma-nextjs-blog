ec4bf1269719f3242d0f2cb5d34bb11d8480aa4c
ruheni <aleruheni@outlook.com>
2022-04-08 18:00:13 +0200

    init

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import { SessionProvider } from "next-auth/react";
+import { AppProps } from "next/app";
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default App;
