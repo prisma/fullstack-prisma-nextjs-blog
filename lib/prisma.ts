8d4c56297999ec5b953d6b0c413bf93acec48c1c
Aman Varshney <amanvarshney.work@gmail.com>
2025-11-18 15:46:52 +0530

    ok

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -8,11 +9,18 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient
 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient({
+    adapter,
+  })
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient()
+    global.prisma = new PrismaClient({
+      adapter,
+    })
   }
   prisma = global.prisma
 }
